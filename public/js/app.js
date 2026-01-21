f9d058edce5b910f8f8f13d8243404e012079767
ChoroukMouaky <choroukmouaky7@gmail.com>
2026-01-21 11:50:28 +0100

    add the about section in html file

---
+++ public/js/app.js
@@ -0,0 +1,64 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//* Testimonials section
+const containers = document.querySelectorAll(".carousel-container");
+
+    containers.forEach(container => {
+
+        const carousel = container.querySelector(".carousel");
+        const slides = container.querySelectorAll(".slide");
+        let index = 0;
+
+        const indicatorsGrp = document.createElement("div");
+        indicatorsGrp.className = "indicators-grp";
+        container.appendChild(indicatorsGrp);
+
+        slides.forEach((slide, i) => {
+            const indicator = document.createElement("div");
+            indicator.className = "indicator";
+            if (i === 0) indicator.classList.add("activeIndicator");
+            indicatorsGrp.appendChild(indicator);
+
+            indicator.addEventListener("click", () => {
+                goToSlide(i);
+            });
+        });
+
+        const indicators = indicatorsGrp.querySelectorAll(".indicator");
+
+        function goToSlide(i) {
+            carousel.style.transform = `translateX(-${i * 100}%)`;
+
+            indicators.forEach(ind => ind.classList.remove("activeIndicator"));
+            indicators[i].classList.add("activeIndicator");
+
+            index = i;
+        }
+        setInterval(() => {
+            index++;
+            if (index >= slides.length) index = 0;
+            goToSlide(index);
+        }, 3000);
+    });
+// !end of Testimonials section
